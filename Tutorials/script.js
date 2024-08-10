b42e57c9337e0bb2eb0282e675287143ba611175
ISRAEL <112627382+realakinolaisrael@users.noreply.github.com>
2024-08-10 12:13:01 +0100

    update

---
+++ Tutorials/script.js
@@ -0,0 +1,24 @@
+// Declarating Variables 
+let message = "Hello, Israel";
+let _message = "Hello My friend Israel"; 
+let $message = "Hello Israel Akinola";
+
+//  console.log(message);
+// console.log(_message);
+// console.log($message);
+
+message = "Hello, Tobi"; 
+
+// console.log(message);
+
+
+// BigInt value
+let value1 = 900719925124740998n;
+let value2 = 900719925124740998n;
+// add two big integers
+let result1 = value1 + 1n + value2;
+console.log(result1);  // "900719925124740999n"
+
+
+
+
